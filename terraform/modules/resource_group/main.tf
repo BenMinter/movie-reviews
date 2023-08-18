24b8fe4d03dbe277219a791cc957b0f7be060d4c
Ben Minter <benminter97@gmail.com>
2023-08-18 22:16:08 +0100

    just use pages

--- terraform/modules/resource_group/main.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "resource_group" {
-  location = var.location
-  name     = var.name
-  tags = merge(var.tags, {
-    "environment" = var.environment
-    "iac:flavour" = "terraform"
-    "component"   = "resource_group"
-  })
-}
